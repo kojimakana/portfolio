7e539862972cc6f5543989eb8ac8b3020ac7404a
kojima <k2knpn@gmail.com>
2021-04-06 04:20:17 +0900

    [Add] LPimageのダイアログ表示

---
+++ src/components/LPimage.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="text-center">
+    <v-card>
+      <v-card-text>
+        <v-img :src="require('@/assets/img/ISARA1.png')"></v-img>
+        <v-img :src="require('@/assets/img/ISARA2.png')"></v-img>
+      </v-card-text>
+
+      <v-divider></v-divider>
+
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn
+          color="primary"
+          text
+          @click.prevent="close"
+        >
+          close
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LPimage',
+  methods: {
+    close() {
+      this.$emit('closeImage')
+    }
+  }
+}
+</script>
